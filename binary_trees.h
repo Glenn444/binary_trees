50027da4bb7fb19a827a34d6801e0aecc49bf534
Glenn444 <gmark1586@gmail.com>
2024-06-06 14:47:00 +0300

    Header file

--- binary_trees.h
+++ binary_trees.h
@@ -17,25 +17,25 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
 
-//Binary Search Tree
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
 
-//AVL Tree
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
 
-//Max Binary Heap
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
-
+/* ------------------  Mandatory task -------------------------*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
